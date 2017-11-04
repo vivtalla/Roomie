8af0766f33b09648d7d4af2e48c252b5ff5b18ae
Vivek Tallavajhala <vivtalla@ku.edu>
2017-11-04 15:44:19 -0500

    Initial Commit

---
+++ Roomie/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Roomie
+//
+//  Created by Vivek Tallavajhala  on 11/4/17.
+//  Copyright © 2017 Monsters Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
