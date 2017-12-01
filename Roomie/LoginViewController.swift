b98aae776e80a7f0078c3e98b1c4befa71f3d53f
Vivek Tallavajhala <vivtalla@ku.edu>
2017-11-30 18:34:07 -0600

    Add Password Reset Functionality to Application

--- Roomie/LoginViewController.swift
+++ Roomie/LoginViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import FirebaseAuth
+import Firebase
 
 class LoginViewController: UIViewController {
 
@@ -59,10 +60,52 @@ class LoginViewController: UIViewController {
         }
     }
     
+    @IBAction func didTapForgotPassword(_ sender: UIButton) {
+        
+        let prompt = UIAlertController(title: "Roomie", message: "Email:", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default) { (action) in
+            let userInput = prompt.textFields![0].text
+            if (userInput!.isEmpty)
+            {
+                return
+            }
+            Auth.auth().sendPasswordReset(withEmail: userInput!) {error in
+                if error != nil {
+                    if let errCode = AuthErrorCode(rawValue: error!._code){
+                 
+                        switch errCode{
+                        case .userNotFound:
+                            let alert = UIAlertController(title: "User Not Found", message: "Try Registering", preferredStyle: UIAlertControllerStyle.alert)
+                            alert.addAction(UIAlertAction(title: "Click", style: UIAlertActionStyle.default, handler: nil))
+                            self.present(alert, animated: true, completion: nil)
+                        default:
+                            let alert = UIAlertController(title: "Error", message: "Something went wrong", preferredStyle: UIAlertControllerStyle.alert)
+                            alert.addAction(UIAlertAction(title: "Click", style: UIAlertActionStyle.default, handler: nil))
+                            self.present(alert, animated: true, completion: nil)
+
+                        }
+                    }
+                }
+                else
+                {
+                    DispatchQueue.main.async{
+                        let alert = UIAlertController(title: "Sweet!", message: "You'll Recieve an email shortly to reset your password", preferredStyle: UIAlertControllerStyle.alert)
+                        alert.addAction(UIAlertAction(title: "Click", style: UIAlertActionStyle.default, handler: nil))
+                        self.present(alert, animated: true, completion: nil)
+                    }
+                }
+            }
+        }
+        prompt.addTextField(configurationHandler: nil)
+        prompt.addAction(okAction)
+        present(prompt, animated: true, completion: nil)
+    }
     
     func signIn() {
         performSegue(withIdentifier: "LoginToHome", sender: nil)
     }
+    
+     @IBAction func unwindToLogin(segue: UIStoryboardSegue) {}
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
