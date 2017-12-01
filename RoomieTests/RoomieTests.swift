17374bca0bf190e1e66e88796c6240cfc869f3bf
mavedantam <31481146+mavedantam@users.noreply.github.com>
2017-11-30 23:08:13 -0600

    Update RoomieTests.swift

--- RoomieTests/RoomieTests.swift
+++ RoomieTests/RoomieTests.swift
@@ -1,18 +1,23 @@
+
 //
-//  RoomieTests.swift
-//  RoomieTests
+//  TestSuite.swift
+//  Roomie
 //
-//  Created by Shreya Lingamallu on 11/30/17.
+//  Created by Menita Vedantam on 11/30/17.
 //  Copyright © 2017 Monsters Inc. All rights reserved.
 //
 
 import XCTest
+@testable import Roomie
 
-class RoomieTests: XCTestCase {
+
+class TestSuite: XCTestCase {
     
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        
     }
     
     override func tearDown() {
@@ -20,9 +25,31 @@ class RoomieTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
+    func testCreateNewAccount() {
+        print("Running tests...")
+        var passed = false
+        let badEmail = "npt"
+        let goodEmail = "menita@gmail.com"
+        let badPassword = "dfg"
+        let goodPassword = "asdfghj"
+        if (!isValidEmail(badEmail) && isValidEmail(goodEmail) && !testPassword(badPassword) && testPassword(goodPassword)){
+            passed = true
+        }
+        if (passed){
+            print("Create New Account: PASSED")
+        }
+        else{
+            print("Create New Account: FAILED")
+        }
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    func testPassword(password: String) ->Bool{
+        if (password.count > 6){
+            return true
+        }
+        else{
+            return false
+        }
     }
     
     func testPerformanceExample() {
